a4e29ff47c646190d66cbc20c3000d9d53ca579f
Andres <web.arod@gmail.com>
2024-03-11 10:05:40 -0500

    desktop finished

---
+++ app/map/MapComponent.jsx
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import GoogleMapReact from 'google-map-react';
+
+// const MapComponent = () => {
+//   const defaultProps = {
+//     center: {
+//       lat: 37.7749, // Set your desired initial latitude
+//       lng: -122.4194, // Set your desired initial longitude
+//     },
+//     zoom: 11, // Set your desired initial zoom level
+//   };
+
+//   return (
+//     <div style={{ height: '500px', width: '100%' }}>
+//       <GoogleMapReact
+//         bootstrapURLKeys={{ key: `${process.env.GOOGLE_MAPS_API_KEY}` }}
+//         defaultCenter={defaultProps.center}
+//         defaultZoom={defaultProps.zoom}
+//       ></GoogleMapReact>
+//     </div>
+//   );
+// };
+
+// export default MapComponent;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
-import Image from 'next/image';
+import Script from 'next/script';
 import './landing.css';
+import MapComponent from './map/MapComponent';
 
 export default function Home() {
   return (
     <main className="main">
+      <Script
+        async
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&callback=console.debug&libraries=maps,marker&v=beta`}
+      ></Script>
       <header>
         <div className="huella">
           <img
@@ -61,6 +66,104 @@ export default function Home() {
           src="/assets/fill.svg"
           alt="relleno de el plato"
         /> */}
+      </section>
+      <section className="quien">
+        <img
+          className="quine-title"
+          src="/assets/quien/quienes.svg"
+          alt="Quien es Arthur?"
+        />
+        <div className="pic-cont-desk">
+          <div className="top-desk">
+            <img
+              className="foto1-desk"
+              src="/assets/quien/foto1.svg"
+              alt="imagen tipo polaroid con arthur en un kayak"
+            />
+            <img
+              className="foto2-desk"
+              src="/assets/quien/foto2.svg"
+              alt="imagen tipo polaroid de arthur mirando a su dueno"
+            />
+            <img
+              className="foto3-desk"
+              src="/assets/quien/foto3.svg"
+              alt="imagen tipo polaroid de arthur con un ocaso"
+            />
+          </div>
+          <div className="bottom-desk">
+            <img
+              className="text1-desk"
+              src="/assets/quien/text1-desk.svg"
+              alt="imagen tipo polaroid de arthur con un ocaso"
+            />
+            <img
+              className="text2-desk"
+              src="/assets/quien/text2-desk.svg"
+              alt="imagen tipo polaroid de arthur con un ocaso"
+            />
+            <img
+              className="text3-desk"
+              src="/assets/quien/text3-desk.svg"
+              alt="imagen tipo polaroid de arthur con un ocaso"
+            />
+            <img
+              className="text4-desk"
+              src="/assets/quien/text4-desk.svg"
+              alt="imagen tipo polaroid de arthur con un ocaso"
+            />
+          </div>
+        </div>
+        <img
+          className="placeholder-desk"
+          src="/assets/quien/placeholder.svg"
+          alt="placeholder"
+        />
+      </section>
+      <section className="trailer">
+        <img
+          className="poster-desk"
+          src="/assets/trailer/cover.svg"
+          alt="placeholder"
+        />
+        <img
+          className="mira-desk"
+          src="/assets/trailer/mira.svg"
+          alt="placeholder"
+        />
+        <div className="iframe-cont">
+          <iframe
+            width="500"
+            height="315"
+            src="https://www.youtube.com/embed/wjDJNEPghNY?si=XVnVpSGE4heRIjS0"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+          <span>ARTHUR | TRAILER OFICIAL</span>
+        </div>
+      </section>
+      <section className="donacion">
+        <img
+          className="dona-desk"
+          src="/assets/donacion/comodonar.svg"
+          alt="placeholder"
+        />
+        <div className="map-cont" id="map">
+          {/* <MapComponent /> */}
+          <gmp-map
+            style={{ borderRadius: '10px', width: '90%', overflow: 'hidden' }}
+            center="-0.1646224409341812,-78.46732330322266"
+            zoom="16"
+            map-id="DEMO_MAP_ID"
+          >
+            <gmp-advanced-marker
+              position="-0.1646224409341812,-78.46732330322266"
+              title="My location"
+            ></gmp-advanced-marker>
+          </gmp-map>
+        </div>
       </section>
     </main>
   );
